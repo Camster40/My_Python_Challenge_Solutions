e34857b4f7be05cf652043b31f81f7e49ccc85cf
Cameron Shaw <Cameron.shaw40@icloud.com>
2025-03-04 15:23:31 +0000

    First try at solving this problem, need to fix repeating numbers

--- Maths/01-Prime_Factor/my_solution.py
+++ Maths/01-Prime_Factor/my_solution.py
@@ -3,8 +3,21 @@ from challenge import Challenge  # Importing the Challenge class
 
 def my_solver(challenge: Challenge):  # Defining your solver
     data = challenge.data  # Storing our challenge data
-    # YOUR SOLVER GOES HERE
-    return ...  # Don't forget to return your solution!
+    factors = []
+
+    while data % 2 == 0:
+        factors.append(2)
+        data //= 2
+
+    for i in range(3, int(data**0.5) + 1, 2):
+        while data % i == 0:
+            factors.append(i)
+            data //= i
+
+    if data > 2:
+        factors.append(data)
+
+    return factors  # Don't forget to return your solution!
 
 
 if __name__ == "__main__":
@@ -13,3 +26,5 @@ if __name__ == "__main__":
     solution = my_solver(my_challenge)  # Running your solver and store what returns
     result = my_challenge.check_solution(solution)  # Checking the solution
     print(f"{raw_data} | {solution} | {result}")  # Print the final outcome
+
+# need to fix so that it does not repeat numbers in the solution
